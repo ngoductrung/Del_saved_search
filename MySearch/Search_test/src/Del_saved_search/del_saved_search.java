5415e7be83d3d3e608ecede651b51f24d4c1ee0c
ngoductrung <72774561+ngoductrung@users.noreply.github.com>
2020-12-30 00:27:49 +0700

    Add files via upload
    done

--- MySearch/Search_test/src/Del_saved_search/del_saved_search.java
+++ MySearch/Search_test/src/Del_saved_search/del_saved_search.java
@@ -63,25 +63,121 @@ public class del_saved_search {
             connection.disconnect();
         }
     }
-    public static void case1() throws IOException {
-        System.out.println("Case 1: Entering ");
-        LoginResp loginResp = getInfoFromServer("0974732000", "1234567");
-        Resp Resp = Del_saved_search(loginResp.data.token, "12","1");
+//    public static void case1() throws IOException {
+//        System.out.println("Case 1: Entering ");
+//        LoginResp loginResp = getInfoFromServer("0974732000", "1234567");
+//        Resp Resp = Del_saved_search(loginResp.data.token, "12","1");
+//        try {
+//            assert "1000".equals(loginResp.data.token) : "Fail" ;
+//            System.out.println("OK");
+//        }
+//        catch (AssertionError e) {
+//            e.printStackTrace();
+//        }
+//    }
+//    public static void case2() throws IOException {
+//        System.out.println("Case 2: Entering ");
+//        LoginResp loginResp = getInfoFromServer("0974732000", "1234567");
+//        Resp resp = Del_saved_search(loginResp.data.token, "12","1");
+//        try {
+//            assert "9998".equals(resp.code) : "Fail";
+//            System.out.println("Wrong token");
+//        }
+//        catch (AssertionError e) {
+//            e.printStackTrace();
+//        }
+//    }
+public static void case1() throws IOException {
+    System.out.println("Case 1: Check all ");
+    LoginResp loginResp = getInfoFromServer("0974732000", "123456");
+    Resp Resp = Del_saved_search(loginResp.data.token, "12","1");
+    try {
+        assert ("1000".equals(loginResp.data.token));
+        System.out.println("OK");
+    }
+    catch (AssertionError e) {
+        e.printStackTrace();
+    }
+}
+    public static void case2() throws IOException {
+        System.out.println("Case 2: Sai token ");
+        LoginResp loginResp = getInfoFromServer("0974732000", "123456");
+        Resp resp = Del_saved_search(loginResp.data.token, "12","1");
         try {
-            assert "1000".equals(loginResp.data.token) : "Fail";
+            assert ("9998".equals(resp.code));
             System.out.println("OK");
         }
         catch (AssertionError e) {
             e.printStackTrace();
         }
     }
-    public static void case2() throws IOException {
-        System.out.println("Case 2: Entering ");
-        LoginResp loginResp = getInfoFromServer("0974732000", "1234567");
+    public static void case3() throws IOException {
+        System.out.println("Case 3: Nhap dung nhung khong co gia tri tra ve");
+        LoginResp loginResp = getInfoFromServer("0974732000", "123456");
         Resp resp = Del_saved_search(loginResp.data.token, "12","1");
         try {
-            assert "9998".equals(resp.code) : "Fail";
-            System.out.println("Wrong token");
+            assert "9994".equals(resp.code) : "Fail";
+            System.out.println("OK");
+        }
+        catch (AssertionError e) {
+            e.printStackTrace();
+        }
+    }
+    public static void case4() throws IOException {
+        System.out.println("Case 4: Kiem tra tham so day du ");
+        LoginResp loginResp = getInfoFromServer("0974732000", "123456");
+        Resp resp = Del_saved_search(loginResp.data.token, "123","1");
+        try {
+            assert "1002".equals(resp.code) : "Fail";
+            System.out.println("OK");
+        }
+        catch (AssertionError e) {
+            e.printStackTrace();
+        }
+    }
+    public static void case5() throws IOException {
+        System.out.println("Case 5: Kiem tra nhap sai thong tin ");
+        LoginResp loginResp = getInfoFromServer("0974732000", "1234567");
+        Resp resp = Del_saved_search(loginResp.data.token, "","1");
+        try {
+            assert "9997".equals(resp.code) : "Fail";
+            System.out.println("OK");
+        }
+        catch (AssertionError e) {
+            e.printStackTrace();
+        }
+    }
+    public static void case6() throws IOException {
+        System.out.println("Case 6: Kiem tra kieu tham so ");
+        LoginResp loginResp = getInfoFromServer("0974732000", "123456");
+        Resp resp = Del_saved_search(loginResp.data.token, "1","1");
+        try {
+            assert "1003".equals(resp.code) : "Fail";
+            System.out.println("OK");
+        }
+        catch (AssertionError e) {
+            e.printStackTrace();
+        }
+    }
+    public static void case7() throws IOException {
+        System.out.println("Case 7: kiem tra bai viet co ton tai khong");
+        LoginResp loginResp = getInfoFromServer("0974732000", "123456");
+        Resp resp = Del_saved_search(loginResp.data.token, "","1");
+        try {
+            assert "9992".equals(resp.code) : "Fail";
+            System.out.println("OK");
+        }
+        catch (AssertionError e) {
+            e.printStackTrace();
+        }
+    }
+    public static void case8() throws IOException {
+        System.out.println("Case 5: Kiem tra gia tri tham so ");
+        LoginResp loginResp = getInfoFromServer("0974732000", "123456");
+        Resp resp = Del_saved_search(loginResp.data.token, "","1");
+        try {
+            assert "1004".equals(resp.code) : "Fail";
+            System.out.println("OK");
         }
         catch (AssertionError e) {
             e.printStackTrace();
@@ -89,6 +185,8 @@ public class del_saved_search {
     }
 
     public static void main(String[] args) throws IOException {
-        case1();
+//        case1();
+//        case6();
+//        case3();
     }
 }
